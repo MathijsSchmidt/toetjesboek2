6a365c0a80e530247ae73d22e78aef32b2b2f89a
MathijsSchmidt <mathijs.schmidt@gmail.com>
2014-06-27 14:25:49 +0200

    ok

--- includes/menu.php
+++ includes/menu.php
@@ -4,8 +4,8 @@ Toetjesboek <hr>
 <br> 
 <ul id="menubalk">
 	<li> <a href="home.php"> Home. </a> </li>
-	<li> <a href="overzicht.php"> Alle grechten. </a> </li>
+	<li> <a href="overzicht.php"> Alle gerechten. </a> </li>
 	<li> <a href="logout.php"> Log uit. </a> </li>
 	<br>
-	<li> <input type="text" value="zoeken" id="search" name="search"> </li>
+	<li> <input type="text" value="zoeken" id="search" name="search"> <input type="submit" value="OK" name="submit" id="ok"></li>
 </ul>
